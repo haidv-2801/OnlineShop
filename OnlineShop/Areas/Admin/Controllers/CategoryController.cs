8a9cab2081994b8d6c53b2c27f1a07a269d264b8
Thanglong2k <63629962+Thanglong2k@users.noreply.github.com>
2021-03-08 21:00:28 +0700

    Add files via upload

--- OnlineShop/Areas/Admin/Controllers/CategoryController.cs
+++ OnlineShop/Areas/Admin/Controllers/CategoryController.cs
@@ -12,22 +12,29 @@ namespace OnlineShop.Areas.Admin.Controllers
     public class CategoryController : BaseController
     {
         // GET: Admin/Category
-        public ActionResult Index()
+        public ActionResult Index(string Search, int page = 1, int pageSize = 2)
         {
-            return View();
+            var dao = new CategoryDao();
+            ViewBag.Search = Search;
+            var model = dao.ListAllPaging(Search, page, pageSize);
+            return View(model);
         }
         public ActionResult Create()
         {
             return View();
         }
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult Create(Category model)
         {
             if (ModelState.IsValid)
             {
                 var currentCulture = Session[CommonConstants.CurrentCulture];
                 model.Language = currentCulture.ToString();
-                var id = new CategoryDao().Insert(model);
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.CreatedBy = session.UserName;
+                model.CreatedDate = DateTime.Now;
+                var id = new CategoryDao().Create(model);
                 if (id > 0)
                 {
                     return RedirectToAction("Index");
@@ -38,6 +45,44 @@ namespace OnlineShop.Areas.Admin.Controllers
                 }
             }
             return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var dao = new CategoryDao();
+            var category = dao.GetByID(id);
+            return View(category);
+        }
+
+        // POST: Admin/Category/Edit/5
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Category model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                // TODO: Add update logic here
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.ModifiedBy = session.UserName;
+                model.ModifiedDate = DateTime.Now;
+                var id = new CategoryDao().Edit(model);
+                return RedirectToAction("Index");
+                
+            }
+
+
+
+            return View();
+
+
+        }
+
+        // GET: Admin/Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            new CategoryDao().Delete(id);
+            SetAlert("Xóa liên hệ thành công", "success");
+            return RedirectToAction("Index");
         }
     }
 }
