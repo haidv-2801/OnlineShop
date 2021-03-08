62fe663d55ef5c0cf4c1e209ed4c52c4b61fdbd0
Thanglong2k <63629962+Thanglong2k@users.noreply.github.com>
2021-03-08 20:56:37 +0700

    Add files via upload

--- OnlineShop/Model/Dao/CategoryDao.cs
+++ OnlineShop/Model/Dao/CategoryDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,6 @@ namespace Model.Dao
         {
             db = new OnlineShopDbContext();
         }
-        public long Insert(Category category)
-        {
-            db.Categories.Add(category);
-            db.SaveChanges();
-            return category.ID;
-        }
         public List<Category> ListAll()
         {
             return db.Categories.Where(x => x.Status == true).ToList();
@@ -27,6 +22,58 @@ namespace Model.Dao
         public ProductCategory ViewDetail(long id)
         {
             return db.ProductCategories.Find(id);
+        }
+        public long Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return category.ID;
+        }
+        public long Edit(Category model)
+        {
+            var category = db.Categories.Find(model.ID);
+            category.Name = model.Name;
+            category.MetaTitle = model.MetaTitle;
+            category.MetaKeywords = model.MetaKeywords;
+            category.MetaDescriptions = model.MetaDescriptions;
+            category.DisplayOrder = model.DisplayOrder;
+            category.SeoTitle = model.SeoTitle;
+            category.ShowOnHome = model.ShowOnHome;
+            category.Status = model.Status;
+            category.Language = model.Language;
+            db.SaveChanges();
+
+            return category.ID;
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var category = db.Categories.Find(id);
+                db.Categories.Remove(category);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                //logging
+                return false;
+            }
+        }
+        public IEnumerable<Category> ListAllPaging(string Search, int page, int pageSize)
+        {
+
+            IQueryable<Category> model = db.Categories;
+            if (!string.IsNullOrEmpty(Search))
+            {
+                model = model.Where(x => x.Name.Contains(Search));//tìm trong cả tên và username . sắp xếp giảm dần || x.Name.Contains(Search)
+            }
+            return model.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);//cài nuget pagelist
+        }
+        public Category GetByID(long id)
+        {
+            return db.Categories.Find(id);
         }
     }
 }
